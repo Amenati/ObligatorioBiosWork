bea576ec62beed7248acb1e55fcabc04359df20d
SebaTattaglia <sebatattaglia@gmail.com>
2025-09-14 20:55:50 -0300

    creacion de esqueleto del proyecto

---
+++ src/main/java/uy/edu/bios/ejemplos/bioswork/BiosworkApplication.java
@@ -0,0 +1,13 @@
+package uy.edu.bios.ejemplos.bioswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiosworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiosworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uy/edu/bios/ejemplos/bioswork/BiosworkApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.edu.bios.ejemplos.bioswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiosworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
