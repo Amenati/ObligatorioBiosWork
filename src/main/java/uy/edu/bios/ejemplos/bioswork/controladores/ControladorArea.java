36b806606c13676aaf76fad10915fc21b19019e2
Alejandro <alecabralburgueno@gmail.com>
2025-09-16 15:59:43 -0300

    carga de carpeta Vistas, css y controladores

---
+++ src/main/java/uy/edu/bios/ejemplos/bioswork/controladores/ControladorArea.java
@@ -0,0 +1,58 @@
+package uy.edu.bios.ejemplos.bioswork.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jakarta.validation.Valid;
+//import uy.edu.bios.ejemplos.bioswork.model.Area;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class ControladorArea {
+
+    @Autowired
+    MessageSource messageSource;
+
+    @GetMapping("/areas/ingresar")
+    public String ingresarArea(@ModelAttribute Area area) {
+        return "areas/ingresar";
+    }
+
+    @PostMapping("/areas/ingresar")
+    public String ingresarArea(@ModelAttribute @Valid Area area, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "areas/ingresar";
+        }
+        
+        model.addAttribute("mensaje", "Área ingresada correctamente");
+
+        return "areas/listar";
+    }
+    
+    @GetMapping("/areas/modificar")
+    public String modificarArea(@ModelAttribute Area area) {
+        return "areas/modificar";
+    }
+
+    @PostMapping("/areas/modificar")
+    public String modificarArea(@ModelAttribute @Valid Area area, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "areas/modificar";
+        }
+        
+        model.addAttribute("mensaje", "Área modificada correctamente");
+
+        return "areas/listar";
+    }
+   
+    
+    
+}
---
+++ src/main/java/uy/edu/bios/ejemplos/bioswork/controladores/ControladorInicio.java
@@ -0,0 +1,23 @@
+package uy.edu.bios.ejemplos.bioswork.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class ControladorInicio {
+    @GetMapping
+    public String inicio() {
+        return "inicio";
+    }
+
+    @GetMapping("/areas/listar")
+    public String mostrarArea() {
+        return "areas/listar";
+    }
+    
+
+    
+}
